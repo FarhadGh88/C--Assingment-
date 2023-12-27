462a66c366f29a02035e6d6f2dc88085f7a96c57
FarhadGh88 <56635688+FarhadGh88@users.noreply.github.com>
2023-12-27 19:33:23 +0400

    Add files via upload

---
+++ ContactManagerApp-master/ContactManagerApp/ContactManager.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagerApp
+{
+    public class ContactManager
+    {
+        private List<ContactInfo> contactList;
+        private const string fileName = "contacts.json";
+        public void LoadData()
+        {
+            if (File.Exists(fileName))
+            {
+                string json = File.ReadAllText(fileName);
+                contactList = JsonConvert.DeserializeObject<List<ContactInfo>>(json);
+            }
+            else
+            {
+                contactList = new List<ContactInfo>();
+            }
+        }
+
+        public void SaveData()
+        {
+            string json = JsonConvert.SerializeObject(contactList, Formatting.Indented);
+            File.WriteAllText(fileName, json);
+        }
+
+        public void AddData()
+        {
+            int newId = contactList.Count + 1;
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Enter email address: ");
+            string emailAddress = Console.ReadLine();
+            Console.Write("Enter address information: ");
+            string address = Console.ReadLine();
+
+            ContactInfo newContact = new ContactInfo
+            {
+                Id=newId,
+                FirstName = firstName,
+                LastName = lastName,
+                PhoneNumber = phoneNumber,
+                EmailAddress = emailAddress,
+                Address = address
+            };
+
+            contactList.Add(newContact);
+        }
+
+      
+        public void ListAllData()
+        {
+            Console.WriteLine("All Contacts:");
+            foreach (var contact in contactList)
+            {
+                Console.WriteLine($"Id:{contact.Id}, Name: {contact.FirstName} {contact.LastName}, Email: {contact.EmailAddress}");
+            }
+            Console.WriteLine();
+        }
+
+        public void DisplayData()
+        {
+            Console.Write("Enter email address to display details: ");
+            string emailAddress = Console.ReadLine();
+
+            ContactInfo contact = contactList.Find(c => c.EmailAddress == emailAddress);
+
+            if (contact != null)
+            {
+                Console.WriteLine($"Id: {contact.Id}");
+                Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email Address: {contact.EmailAddress}");
+                Console.WriteLine($"Address: {contact.Address}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+
+        public void DeleteData()
+        {
+            Console.Write("Enter email address to delete contact: ");
+            string emailAddress = Console.ReadLine();
+
+            ContactInfo contactToRemove = contactList.Find(c => c.EmailAddress == emailAddress);
+
+            if (contactToRemove != null)
+            {
+                contactList.Remove(contactToRemove);
+                Console.WriteLine("Contact deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+    }
+}
---
+++ ContactManagerApp-master/ContactManagerApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Tex;
+using System.Threading.Tasks;
+
+namespace ContactManagerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactManager contactManager = new ContactManager();
+
+            contactManager.LoadData();
+            contactManager.ListAllData();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add a contact");
+                Console.WriteLine("2. List all contacts");
+                Console.WriteLine("3. Display detailed information about a contact");
+                Console.WriteLine("4. Delete a contact");
+                Console.WriteLine("5. Exit an SaveData");
+                Console.Write("Select an option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        contactManager.AddData();
+                        break;
+                    case "2":
+                        contactManager.ListAllData();
+                        break;
+                    case "3":
+                        contactManager.DisplayData();
+                        break;
+                    case "4":
+                        contactManager.DeleteData();
+                        break;
+                    case "5":
+                        contactManager.SaveData();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
